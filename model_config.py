aa7339274bbc2628ff2f84477faeb18ef9ffc1cf
gui-shuyue <guishy3@mail2.sysu.edu.cn>
2026-03-16 17:55:42 +0800

    first commit

---
+++ model_config.py
@@ -0,0 +1,10 @@
+# $/M token
+MODELS_CONFIG = {
+    "qwen/qwen-2.5-7b-instruct": {"input_cost": 0.04, "output_cost": 0.10},
+    "meta-llama/llama-3.1-8b-instruct": {"input_cost": 0.02, "output_cost": 0.05},
+    "mistralai/mistral-small-3.2-24b-instruct": {"input_cost": 0.06, "output_cost": 0.18},
+    "google/gemma-3-27b-it": {"input_cost": 0.03, "output_cost": 0.11},
+    "meta-llama/llama-3.3-70b-instruct": {"input_cost": 0.10, "output_cost": 0.32},
+    "qwen/qwen3.5-122b-a10b": {"input_cost": 0.26, "output_cost": 2.08},
+    "mistralai/mixtral-8x22b-instruct": {"input_cost": 2.00, "output_cost": 6.00},
+}
